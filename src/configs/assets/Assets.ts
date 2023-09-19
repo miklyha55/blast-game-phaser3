fe8fa2df49b84dea32faa7002125eb1f16babfa0
Roman Kolenko <miklyha55@yandex.ru>
2023-09-19 10:04:24 +0300

    feat(configs): load assets

--- src/configs/assets/Assets.ts
+++ src/configs/assets/Assets.ts
@@ -6,4 +6,54 @@ export const Assets: ReadonlyArray<IROResourceCfg> = [
         path: "assets/jsons/game.json",
         type: "json",
     },
+    {
+        name: "bg",
+        path: "assets/images/game/bg.png",
+        type: "image",
+    },
+    {
+        name: "btnShort",
+        path: "assets/images/ui/btn_short.png",
+        type: "image",
+    },
+    {
+        name: "panelBuster",
+        path: "assets/images/ui/panel_buster.png",
+        type: "image",
+    },
+    {
+        name: "panelScore",
+        path: "assets/images/ui/panel_score.png",
+        type: "image",
+    },
+    {
+        name: "iconBomb",
+        path: "assets/images/ui/icons/icon_bomb.png",
+        type: "image",
+    },
+    {
+        name: "blue",
+        path: "assets/images/game/blocks/blue.png",
+        type: "image",
+    },
+    {
+        name: "green",
+        path: "assets/images/game/blocks/green.png",
+        type: "image",
+    },
+    {
+        name: "pink",
+        path: "assets/images/game/blocks/pink.png",
+        type: "image",
+    },
+    {
+        name: "red",
+        path: "assets/images/game/blocks/red.png",
+        type: "image",
+    },
+    {
+        name: "yellow",
+        path: "assets/images/game/blocks/yellow.png",
+        type: "image",
+    },
 ];
