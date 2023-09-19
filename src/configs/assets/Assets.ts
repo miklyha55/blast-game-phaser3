129e816560b4d004c9071bafbd615424f5b9a692
Roman Kolenko <miklyha55@yandex.ru>
2023-09-19 11:21:29 +0300

    feat(configs): add enum name assets

--- src/configs/assets/Assets.ts
+++ src/configs/assets/Assets.ts
@@ -1,58 +1,71 @@
 import { IROResourceCfg } from "./types";
+export const enum ASSETS_NAME {
+    GameCfg = "gameCfg",
+    Bg = "bg",
+    BtnShort = "btnShort",
+    PanelBuster = "panelBuster",
+    PanelScore = "panelScore",
+    IconBomb = "iconBomb",
+    BlueBlock = "blue",
+    GreenBlock = "green",
+    PinkBlock = "pink",
+    RedBlock = "red",
+    YellowBlock = "yellow",
+}
 
 export const Assets: ReadonlyArray<IROResourceCfg> = [
     {
-        name: "game.json",
+        name: ASSETS_NAME.GameCfg,
         path: "assets/jsons/game.json",
         type: "json",
     },
     {
-        name: "bg",
+        name: ASSETS_NAME.Bg,
         path: "assets/images/game/bg.png",
         type: "image",
     },
     {
-        name: "btnShort",
+        name: ASSETS_NAME.BtnShort,
         path: "assets/images/ui/btn_short.png",
         type: "image",
     },
     {
-        name: "panelBuster",
+        name: ASSETS_NAME.PanelBuster,
         path: "assets/images/ui/panel_buster.png",
         type: "image",
     },
     {
-        name: "panelScore",
+        name: ASSETS_NAME.PanelScore,
         path: "assets/images/ui/panel_score.png",
         type: "image",
     },
     {
-        name: "iconBomb",
+        name: ASSETS_NAME.IconBomb,
         path: "assets/images/ui/icons/icon_bomb.png",
         type: "image",
     },
     {
-        name: "blue",
+        name: ASSETS_NAME.BlueBlock,
         path: "assets/images/game/blocks/blue.png",
         type: "image",
     },
     {
-        name: "green",
+        name: ASSETS_NAME.GreenBlock,
         path: "assets/images/game/blocks/green.png",
         type: "image",
     },
     {
-        name: "pink",
+        name: ASSETS_NAME.PinkBlock,
         path: "assets/images/game/blocks/pink.png",
         type: "image",
     },
     {
-        name: "red",
+        name: ASSETS_NAME.RedBlock,
         path: "assets/images/game/blocks/red.png",
         type: "image",
     },
     {
-        name: "yellow",
+        name: ASSETS_NAME.YellowBlock,
         path: "assets/images/game/blocks/yellow.png",
         type: "image",
     },
