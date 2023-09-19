7cc176ff81ab38ccae5a6d3801810d38cd2108d0
Roman Kolenko <miklyha55@yandex.ru>
2023-09-19 12:05:39 +0300

    refactor(managers): grid props, handlers, inputs, position

--- src/managers/grid/Cell.ts
+++ src/managers/grid/Cell.ts
@@ -3,6 +3,7 @@ import { IROContextCfg } from '../../scenes/types';
 import { Sprite } from '../../components/sprite/Sprite';
 import { RENDER_LAYERS_NAME } from '../render/constants';
 import { ASSETS_NAME } from '../../configs/assets/Assets';
+import { JoystickCommand } from '../../inputs/commands/JoystickCommand';
 
 export class Cell extends GameObject {
     constructor(context: IROContextCfg) {
@@ -14,9 +15,14 @@ export class Cell extends GameObject {
                 new Sprite({
                     name: "Sprite",
                     scene: context.scenes.gameScene,
-                    texture: ASSETS_NAME.Bg,
+                    texture: ASSETS_NAME.BlueBlock,
+                    origin: {x: 0.5, y: 0.5},
                 })
             ],
         })
+    }
+
+    override onCreate() {
+        new JoystickCommand(this.container);
     }
 }
--- src/managers/grid/GridManager.ts
+++ src/managers/grid/GridManager.ts
@@ -2,16 +2,18 @@ import * as Phaser from 'phaser';
 
 import { IROGridCfg } from "./types";
 import { IVec2 } from '../../utils/types';
+import { Cell } from './Cell';
+import { IROContextCfg } from '../../scenes/types';
 
 export class GridManager {
     private readonly props: IROGridCfg;
-    private readonly scene: Phaser.Scene;
+    private readonly context: IROContextCfg;
     private readonly container: Phaser.GameObjects.Container;
 
-    constructor(scene: Phaser.Scene, props: IROGridCfg) {
+    constructor(context: IROContextCfg, props: IROGridCfg) {
         this.props = props;
-        this.scene = scene;
-        this.container = this.scene.add.container(0, 0);
+        this.context = context;
+        this.container = this.context.scenes.gameScene.add.container(0, 0);
 
         this.createGrid();
     }
@@ -22,10 +24,12 @@ export class GridManager {
         for (let col = 0; col < this.props.col; col++) {
             for (let row = 0; row < this.props.row; row++) {
                 const position: IVec2 = {
-                    x: col * this.props.size,
-                    y: row * this.props.size,
+                    x: col * this.props.size + this.props.size / 2,
+                    y: row * this.props.size + this.props.size / 2,
                 }
+                const cell: Cell = new Cell(this.context);
 
+                cell.container.setPosition(position.x, position.y);
                 index++;
             }
         }
