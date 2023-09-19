8ac3bdb77643e189040ab5a5d62f7bb7c691c3a3
Roman Kolenko <miklyha55@yandex.ru>
2023-09-19 11:20:30 +0300

    refactor(components): sprite props, handlers, container

--- src/components/sprite/Sprite.ts
+++ src/components/sprite/Sprite.ts
@@ -9,16 +9,18 @@ export class Sprite extends Component
     sprite: Phaser.GameObjects.Sprite;
 
     constructor(props: IROSpriteCfg) {
-        super(props, props.scene.add.container(0, 0));
+        super(props);
 
         this.sprite = props.scene.add.sprite(0, 0, props.texture);
         this.container.add(this.sprite);
-
-        this.setEvents();
     }
 
-    private setEvents() {
-        this.container.on(COMPONENT_EVENTS.SET_TEXTURE, this.onSetTexture.bind(this));
+    override onCreate() {
+        this.container.on(COMPONENT_EVENTS.SET_TEXTURE, this.onSetTexture, this);
+    }
+
+    override onRemove() {
+        this.container.off(COMPONENT_EVENTS.SET_TEXTURE, this.onSetTexture, this);
     }
 
     private onSetTexture(key: string) {
