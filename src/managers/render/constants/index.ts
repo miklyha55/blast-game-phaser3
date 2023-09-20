2ecb7f3cc4be7f8a7fa80d797ba196955469943e
Roman Kolenko <miklyha55@yandex.ru>
2023-09-20 10:33:18 +0300

    feat(managers) render constants

--- src/managers/render/constants/index.ts
+++ src/managers/render/constants/index.ts
@@ -1,9 +1,11 @@
 export const enum RENDER_LAYERS_NAME {
     Blocks = "Blocks",
+    Bg = "Bg",
 }
 
 export const RENDER_GAME_LAYERS: string[] = [
-    RENDER_LAYERS_NAME.Blocks
+    RENDER_LAYERS_NAME.Bg,
+    RENDER_LAYERS_NAME.Blocks,
 ]
 
 export const RENDER_UI_LAYERS: string[] = [
