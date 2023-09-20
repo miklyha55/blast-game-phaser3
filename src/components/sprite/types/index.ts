0c37793d6c13dab04c86fddf26703b5c190b32e2
Roman Kolenko <miklyha55@yandex.ru>
2023-09-20 12:17:37 +0300

    refactor(conmponents) sprite types

--- src/components/sprite/types/index.ts
+++ src/components/sprite/types/index.ts
@@ -2,6 +2,6 @@ import { IVec2 } from "../../../utils/types";
 import { IROComponentCfg } from "../../core/types";
 
 export interface IROSpriteCfg extends IROComponentCfg {
-    texture: string;
-    origin?: IVec2;
+    readonly texture: string;
+    readonly origin?: IVec2;
 }
