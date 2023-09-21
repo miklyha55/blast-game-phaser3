d0c2347a63255e45a56891143d3f15f03ae64bb0
Roman Kolenko <miklyha55@yandex.ru>
2023-09-21 13:00:22 +0300

    feat(ui) create button bomb

---
+++ src/ui/ButtonBomb.ts
@@ -0,0 +1,61 @@
+import { ButtonBombCommand } from "../components/input/commands/ButtonBombCommand";
+import { Resize } from "../components/resize/Resize";
+import { Sprite } from "../components/sprite/Sprite";
+import { Text } from "../components/text/Text";
+import { ASSETS_NAME } from "../configs/assets/Assets";
+import { GameObject } from "../managers/gameObject/GameObject";
+import { IROPrefabCfg } from "../managers/gameObject/types";
+import { RENDER_LAYERS_NAME } from "../managers/render/constants";
+
+export class ButtonBomb {
+    gameObject: GameObject;
+
+    constructor(props: IROPrefabCfg) {
+        const { width: gameWidth, height: gameHeidth }
+            = props.context.scenes.gameScene.sys.game.canvas;        
+
+        this.gameObject = props.context.gameObjectManager.createGameObject(
+            {
+                name: "ButtonBomb",
+                scene: props.context.scenes.hudScene,
+                conponents: [
+                    new Sprite({
+                        name: "Sprite",
+                        scene: props.context.scenes.hudScene,
+                        texture: ASSETS_NAME.PanelBuster,
+                    }),
+                    new Sprite({
+                        name: "Sprite",
+                        scene: props.context.scenes.hudScene,
+                        texture: ASSETS_NAME.IconBomb,
+                        position: {
+                            x: 17,
+                            y: -65,
+                        },
+                    }),
+                    new Resize({
+                        name: "Resize",
+                        scene: props.context.scenes.hudScene,
+                        position: {
+                            x: gameWidth / 2 + 650,
+                            y: gameHeidth / 2 + 400,
+                        },
+                    }),
+                    new Text({
+                        name: "Points",
+                        scene: props.context.scenes.hudScene,
+                        text: props.context.jsonGame.bombCount + "",
+                        size: 90,
+                        position: {
+                            x: 0,
+                            y: 110,
+                        },
+                    }),
+                    new ButtonBombCommand(props.context),
+                ],
+                context: props.context,
+                renderLayer: props.context.renderUiManager.getLayerByName(RENDER_LAYERS_NAME.GameUi),
+            }
+        )
+    }
+}
